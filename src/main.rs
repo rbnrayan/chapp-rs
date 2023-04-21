153dfc82a5518e190ac55b0f09deebfb2b1c8b2e
Rayan Robion <rayan.robion7@gmail.com>
2023-04-21 22:13:11 +0200

    READONLY clients functionnality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::env;
-use std::process::exit;
+use std::{env, process::exit};
+use std::io::{self, Write};
 
 mod cli_arg;
 use cli_arg::TypeOption;
-
-// mod thread_pool;
 
 mod chapp_server;
 mod chapp_client;
@@ -16,21 +14,71 @@ fn main() {
 
     match cli_arg::parse_args(args).unwrap() {
         TypeOption::LISTEN(ip) => {
-            ChappServer::new(&ip.to_string()).listen().unwrap();
+            if let Err(e) = ChappServer::new(&ip.to_string()).listen() {
+                eprintln!("SERVER: failed to listen on {:?}: {:?}", ip, e);
+                exit(1);
+            }
         },
         TypeOption::READONLY(ip) => {
             let mut reader = ChappClient::connect(ip);
-            reader.send(b"READER").unwrap();
+
+            if let Err(e) = reader.send(b"READER") {
+                eprintln!("READONLY client: failed to send message: {:?}", e);
+                exit(1);
+            }
+
+            loop {
+                match reader.read() {
+                    Ok((bytes_read, data)) => {
+                        if bytes_read == 0 {
+                            println!("Stream closed");
+                            break;
+                        } else {
+                            // RECEIVED DATA:
+                            // <ip_address>:<port>\ndata\n[\0..]
+                            let mut data_split = data.split(|c| *c == b'\n');
+
+                            let from: &[u8] = data_split.next().unwrap();
+                            let data: &[u8] = data_split.next().unwrap();
+
+                            let from = String::from_utf8(from.to_vec()).unwrap();
+                            let stringified_data = String::from_utf8(data.to_vec()).unwrap();
+                            let stringified_data = stringified_data.strip_suffix("\n")
+                                .unwrap_or(&stringified_data);
+
+                            let msg = format!("@[{}]: {}", from, stringified_data);
+
+                            println!(" > {}", msg);
+                        }
+                    },
+                    Err(e) => {
+                        eprintln!("Failed to read bytes: {:?}", e);
+                        exit(1);
+                    },
+                }
+            }
         }
         TypeOption::WRITEONLY(ip) => {
             let mut writer = ChappClient::connect(ip);
-            writer.send(b"WRITER").unwrap();
+            
+            if let Err(e) = writer.send(b"WRITER") {
+                eprintln!("WRITEONLY client: failed to send message: {:?}", e);
+                exit(1);
+            }
 
             let mut buf = String::new();
 
             while &buf[..] != "q" {
-                std::io::stdin().read_line(&mut buf).unwrap();
-                writer.send(buf.as_bytes()).unwrap();
+                print!(" > ");
+
+                io::stdout().flush().unwrap();
+                io::stdin().read_line(&mut buf).expect("Failed to read line from stdin");
+
+                if let Err(e) = writer.send(buf.as_bytes()) {
+                    eprintln!("WRITEONLY client: failed to send message: {:?}", e);
+                    exit(1);
+                }
+
                 buf.clear();
             }
         },
